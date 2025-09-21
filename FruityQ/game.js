307f67a0a35838ec92e0f1ed2457f5e79d030ecf
ideaqlabs <ideaq.labs@gmail.com>
2025-09-21 19:47:45 +0530

    Update game.js

--- FruityQ/game.js
+++ FruityQ/game.js
@@ -1,1 +1,10 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const startOverlay = document.getElementById("startOverlay");
+const startBtn = document.getElementById("startBtn");
+const gameOverOverlay = document.getElementById("gameOverOverlay");
+const finalScore = document.getElementById("finalScore");
+const restartBtn = document.getElementById("restartBtn");
+const exitBtn = document.getElementById("exitBtn");
 
