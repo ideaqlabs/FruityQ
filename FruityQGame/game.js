d2766bf97293e34c40e2f9ef9ec4e2796a94f3f1
ideaqlabs <ideaq.labs@gmail.com>
2025-09-28 23:03:58 +0530

    Update game.js

--- FruityQGame/game.js
+++ FruityQGame/game.js
@@ -1,3 +1,20 @@
+// ---------- Firebase Initialization ----------
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firebase services
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 (function () {
   const canvas = document.getElementById("gameCanvas");
   const ctx = canvas.getContext("2d");
